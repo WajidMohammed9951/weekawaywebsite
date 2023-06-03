b7eb61e3afe61cc19a9d5d04504a305d716278ed
Mohammed Wajid <wajid17023@gmail.com>
2023-06-03 16:00:13 +0530

    styling header section

--- weekawaywebsite/src/Components/Header.js
+++ weekawaywebsite/src/Components/Header.js
@@ -4,22 +4,22 @@ import { BsChatSquareDots } from 'react-icons/bs';
 
 const Header = () => {
     return (
-        <div className='flex justify-between items-center px-4 py-2'>
+        <div className='flex justify-between items-center px-4 py-4 bg-blue-100'>
             <div className='flex items-center'>
                 <BsChatSquareDots size={30} className=' mr-2' />
-                <h1 className='text-xl font-bold text-gray-700'>WEEKAWAY</h1>
+                <h1 className='text-2xl font-bold text-purple-950'>WEEKAWAY</h1>
             </div>
             <div className='flex'>
-                <div className='hidden md:flex items-center px-6'>
-                    <AiOutlineClockCircle size={20} className='mr-2 ' />
-                    <p className='text-sm text-gray-700'>9AM - 5AM</p>
+                <div className='hidden md:flex items-center px-3'>
+                    <AiOutlineClockCircle size={30} className='mr-1' />
+                    <p className=' text-purple-950 text-xl mr-1'>9AM - 5AM</p>
                 </div>
 
-                <div className='hidden md:flex items-center px-6'>
-                    <AiFillPhone size={20} className='mr-2 ' />
-                    <p className='text-sm text-gray-700'>1-888-817-1234</p>
+                <div className='hidden md:flex items-center px-3'>
+                    <AiFillPhone size={30} className='mr-1 ' />
+                    <p className=' text-purple-950 text-xl mr-1'>1-888-817-1234</p>
                 </div>
-                <button className='text-blue-400'>Get a Free Quote</button>
+                <button className='text-xl mr-3 ml-6 border-0  font-sans  bg-blue-500 px-2 py-2 rounded-lg border-b-2 text-black'>Get a Free Quote</button>
 
 
             </div>
