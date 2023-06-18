2a060102346313a341b8dff8f0549b56a873cff8
Mohammed Wajid <wajid17023@gmail.com>
2023-06-18 14:49:18 +0530

    adding elements in navbar

--- weekawaywebsite/src/App.js
+++ weekawaywebsite/src/App.js
@@ -1,10 +1,12 @@
  import React from "react";
 import Header from "./Components/Header";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div >
       <Header/>
+      <Navbar/>
     </div>
   );
 }
--- weekawaywebsite/src/Components/Navbar.js
+++ weekawaywebsite/src/Components/Navbar.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { icons } from 'react-icons/lib'
+import React,{useState, useEffect} from 'react'
 
-import { FaBars } from 'react-icons/fa'
 import { BsChatSquareDots } from 'react-icons/bs'
 import {
     FaFacebookF,
     FaTwitter,
     FaGooglePlusG,
     FaInstagram,
+    FaBars,
+    
   } from 'react-icons/fa';
 
 
 const Navbar = () => {
   return (
-    <div className='flex'>
-        <ul>
+    <div className=''>
+        <ul className='mx-2'>
             <li>
                 <a href='/' >Home</a>
             </li>
@@ -28,6 +28,32 @@ const Navbar = () => {
                 <a href='#contact' >Contact</a>
             </li>
         </ul>
+        <div>
+            <FaFacebookF className='mx-2'/>
+            <FaTwitter className='mx-2'/>
+            <FaGooglePlusG className='mx-2'/>
+            <FaInstagram className='mx-2'/>
+        </div>
+
+        <div>
+            <FaBars className='mx-2'/>
+        </div>
+        <div>
+        <ul className='mx-2'>
+            <li>
+                <a href='/' >Home</a>
+            </li>
+            <li>
+                <a href='#gallery' >Gallery</a>
+            </li>
+            <li>
+                <a href='#deals' >Deals</a>
+            </li>
+            <li>
+                <a href='#contact' >Contact</a>
+            </li>
+        </ul>
+        </div>
     </div>
   )
 }
