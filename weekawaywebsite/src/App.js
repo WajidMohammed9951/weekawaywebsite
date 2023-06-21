86f9dff13346f4026a837804d32e7f026de4f26a
Mohammed Wajid <wajid17023@gmail.com>
2023-06-21 13:35:27 +0530

    adding elements in hero section

--- weekawaywebsite/src/App.js
+++ weekawaywebsite/src/App.js
@@ -1,12 +1,14 @@
  import React from "react";
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
+import HeroSection from "./Components/HeroSection";
 
 function App() {
   return (
     <div >
       <Header/>
       <Navbar/>
+      <HeroSection/>
     </div>
   );
 }
---
+++ weekawaywebsite/src/Components/HeroSection.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div className='w-full h-[90vh]'>
+        <img
+        src='https://images.unsplash.com/photo-1505228395891-9a51e7e86bf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3433&q=80'
+        alt='/'
+        className='w-full h-full object-cover'
+      />
+      <div>
+        <h1>find your trip</h1>
+        <h2>with weekaway</h2>
+      </div>
+      <p>If you want ro travel over the world, weekaway can provide you suggestions on how you can plan your trips.It shows number of destinations where you want to travel across the world, It can provides all sources of images of destionations.
+      </p>
+    </div>
+  )
+}
+
+export default HeroSection
