b3df5bb9310cf8ee833b9bd0b1ebcad9c330469e
Mohammed Wajid <wajid17023@gmail.com>
2023-06-22 23:38:00 +0530

    adding elements and styling to gallery section

--- weekawaywebsite/src/App.js
+++ weekawaywebsite/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./Components/Navbar";
 import HeroSection from "./Components/HeroSection";
 import Activities from "./Components/Activities";
 import Booking from "./Components/Booking";
+import Gallery from "./Components/Gallery";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <HeroSection/>
       <Activities/>
       <Booking/>
+      <Gallery/>
     </div>
   );
 }
