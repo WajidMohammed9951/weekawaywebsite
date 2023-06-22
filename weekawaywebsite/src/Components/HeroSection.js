e78da8ca5bc6b7c10d510caf53bebc00e7f41e8d
Mohammed Wajid <wajid17023@gmail.com>
2023-06-22 16:26:11 +0530

    creating activities section

--- weekawaywebsite/src/Components/HeroSection.js
+++ weekawaywebsite/src/Components/HeroSection.js
@@ -10,11 +10,10 @@ const HeroSection = () => {
             />
             <div className='max-w-[1140px] m-auto'>
                 <div className='absolute top-[40%] w-full md:[50%] flex flex-col text-white p-4'>
-                    <h1>find your trip</h1>
-                    <h2>with weekaway</h2>
+                    <h1 className='font-bold text-4xl'>Find your trip</h1>
+                    <h2 className='text-4xl py-4 '>With weekaway</h2>
                     
-
-                    <p>If you want ro travel over the world, weekaway can provide you suggestions on how you can plan your trips.It shows number of destinations where you want to travel across the world, It can provides all sources of images of destionations.
+                    <p>If you want ro travel over the world, weekaway can provide you suggestions on how you can plan your trips.It shows number of destinations where you want to travel across the world, It can provides all sources of images of destionations.You only need this weekaway app to choose your destionations.
                     </p>
                 </div>
 
